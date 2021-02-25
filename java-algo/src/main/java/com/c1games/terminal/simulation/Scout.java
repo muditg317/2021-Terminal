b86ae8904af2ff4647e9bc347c823234d6b35ff9
Mudit Gupta <muditg317@gmail.com>
2021-02-25 11:27:09 -0500

    a bunch of setup for the simulator

--- java-algo/src/main/java/com/c1games/terminal/simulation/Scout.java
+++ java-algo/src/main/java/com/c1games/terminal/simulation/Scout.java
@@ -4,6 +4,8 @@ import com.c1games.terminal.algo.Coords;
 import com.c1games.terminal.algo.PlayerId;
 import com.c1games.terminal.algo.map.Unit;
 
+import java.util.function.Function;
+
 public class Scout extends MobileUnit {
   private static final double startHealth = 15;
   private static final double range = 3.5; //max euclidean distance
@@ -11,8 +13,16 @@ public class Scout extends MobileUnit {
   private static final double structureDamage = 2;
   private static final double speed = 1; //num frames to move
 
+  private final Function<SimUnit, Boolean> interactiblityFilter;
+
   public Scout(Unit scout, Coords location) {
     super(scout.owner == PlayerId.Player2, scout.id, scout.unitInformation.startHealth.orElse(Scout.startHealth), scout.unitInformation.attackRange.orElse(Scout.range), scout.unitInformation.attackDamageWalker.orElse(Scout.walkerDamage), scout.unitInformation.attackDamageTower.orElse(Scout.structureDamage), scout.unitInformation.speed.orElse(Scout.speed), location, scout.health, Direction.SPAWNED);
+    interactiblityFilter = simUnit -> simUnit.isEnemy != this.isEnemy;
+  }
+
+  @Override
+  public Function<SimUnit, Boolean> getInteractabilityFilter() {
+    return interactiblityFilter;
   }
 
 
